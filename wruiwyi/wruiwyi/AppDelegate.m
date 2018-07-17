85931c467da7f749e5ece3b5fb26a154d45b140d
houyuxin <784929471@qq.com>
2018-07-17 23:16:41 +0800

    1

--- wruiwyi/wruiwyi/AppDelegate.m
+++ wruiwyi/wruiwyi/AppDelegate.m
@@ -17,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    //sdfafdadfafdasfsw234235356467657
     return YES;
 }
 
