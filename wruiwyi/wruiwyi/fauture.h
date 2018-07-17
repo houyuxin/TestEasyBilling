f03800637ca46a106c7361a86fa6943de6ac5d18
houyuxin <784929471@qq.com>
2018-07-17 23:45:05 +0800

    fature1111

---
+++ wruiwyi/wruiwyi/fauture.h
@@ -0,0 +1,13 @@
+//
+//  fauture.h
+//  wruiwyi
+//
+//  Created by hyx on 2018/7/17.
+//  Copyright © 2018年 HYX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface fauture : NSObject
+
+@end
---
+++ wruiwyi/wruiwyi/fauture.m
@@ -0,0 +1,13 @@
+//
+//  fauture.m
+//  wruiwyi
+//
+//  Created by hyx on 2018/7/17.
+//  Copyright © 2018年 HYX. All rights reserved.
+//
+
+#import "fauture.h"
+
+@implementation fauture
+
+@end
